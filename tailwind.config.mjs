290d776b62bd2be6fe7876f39e57c7abfa67d17c
Programador <vtex@subeagenciadigital.com>
2025-02-27 10:26:46 +0100

    feat: create global styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3490dc", // Replace with your desired primary color
+      },
+    },
+  },
+  plugins: [],
+};
